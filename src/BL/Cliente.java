f3bc1f6809004770c40f7d65d9eec76bd1380775
michael <michael@michael-SVF14213CLB>
2017-04-17 15:44:48 -0600

    Se añadio la clase Cliente.

---
+++ src/BL/Cliente.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BL;
+
+/**
+ *
+ * @author michael
+ */
+public class Cliente {
+    
+    String Nombre;
+    String PrimerApellido;
+    String SegundoApellido;
+
+    public Cliente(String Nombre, String PrimerApellido, String SegundoApellido) {
+        this.Nombre = Nombre;
+        this.PrimerApellido = PrimerApellido;
+        this.SegundoApellido = SegundoApellido;
+    }
+    
+    
+    
+}
