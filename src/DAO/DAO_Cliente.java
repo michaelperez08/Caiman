e9c5d078fc3ce81e0f6fb904fcfcb166f8e9a3d7
Extreme PC <Extreme PC@LAPTOP-T5DF151T>
2017-04-20 11:20:49 -0600

    Creacion de logica de DaoCliente

--- src/DAO/DAO_Cliente.java
+++ src/DAO/DAO_Cliente.java
@@ -19,5 +19,38 @@ import java.util.logging.Logger;
  * @author michael
  */
 public class DAO_Cliente {
+      private Connection conexion;
+    private DAO_Conexion daoConexion = new DAO_Conexion();
+    private PreparedStatement cmd;
+    private ResultSet rs;
+    private TO_Cliente cliente;
     
+    public boolean ingresarCliente(String nombre,String direccion,String cedula){
+        
+      try {
+          if(conexion == null || conexion.isClosed()){
+           conexion = daoConexion.nuevaConexion();
+          }
+          
+         
+                
+      cmd = conexion.prepareStatement("insert into Cliente(Nombre,Direccion,Cedula) values (?,?,?,?);");
+      cmd.setString(2,nombre);
+      cmd.setString(3,direccion);
+      cmd.setString(4,cedula);
+      cmd.execute();
+      return true;
+      
+      } catch (SQLException ex) {
+          Logger.getLogger(DAO_Cliente.class.getName()).log(Level.SEVERE, null, ex);
+      }finally{
+          try {    
+              conexion.close();
+          } catch (SQLException ex) {
+              Logger.getLogger(DAO_Cliente.class.getName()).log(Level.SEVERE, null, ex);
+          }
+      }
+      
+      return false;
+    }
 }
