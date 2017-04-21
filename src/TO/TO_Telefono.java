48760fb7ca79d897658ac5f46a975c4265739565
Extreme PC <Extreme PC@LAPTOP-T5DF151T>
2017-04-21 11:16:30 -0600

    Creacion Clase TO_Telefono

--- src/TO/TO_Telefono.java
+++ src/TO/TO_Telefono.java
@@ -10,5 +10,31 @@ package TO;
  * @author Extreme PC
  */
 public class TO_Telefono {
+    int id;
+    String Telefono;
     
+    public TO_Telefono(String Telefono){
+        this.Telefono = Telefono;
+    }
+     public TO_Telefono(int id,String Telefono){
+        this.Telefono = Telefono;
+    }
+    public TO_Telefono(){
+        
+    }
+    
+    public String getTelefono(){
+        return Telefono;
+    } 
+    
+    public void setTelefono(String Telefono){
+       this.Telefono = Telefono; 
+    }
+      public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
