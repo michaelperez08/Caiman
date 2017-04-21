bdbe6e24f5f1404e59f04a730b493476589041dd
Extreme PC <Extreme PC@LAPTOP-T5DF151T>
2017-04-21 10:56:08 -0600

    Creacion TO_Cliente

--- src/TO/TO_Cliente.java
+++ src/TO/TO_Cliente.java
@@ -10,9 +10,56 @@ package TO;
  * @author michael
  */
 public class TO_Cliente {
+    int id; 
+    String Nombre;
+    String Direccion;
+    String Cedula;
     
-    public String nombre;
-    public String primerApellido;
-    public String segundoApellido;
     
+    public TO_Cliente(String Nombre,String Direccion, String cedula){
+        this.Nombre = Nombre;
+        this.Direccion = Direccion;
+        this.Cedula = cedula;
+    }
+    public TO_Cliente(int id ,String Nombre,String Direccion, String cedula){
+        this.id = id;
+        this.Nombre = Nombre;
+        this.Direccion = Direccion;
+        this.Cedula = cedula;
+    }
+         public TO_Cliente(){
+            
+        }
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+    
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+    
+    public String getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(String Cedula) {
+        this.Cedula = Cedula;
+    }
+    
+      public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
