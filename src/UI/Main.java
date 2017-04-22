673c8fe01c27c61fe02ac50458a2b1520b037345
Extreme PC <Extreme PC@LAPTOP-T5DF151T>
2017-04-22 16:55:00 -0600

    Error comentado BD

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -33,9 +33,9 @@ public class Main {
         } catch (Exception e) {
     // If Nimbus is not available, you can set the GUI to another look and feel.
         }
-          /*
+         
         ConfBD confdb = new ConfBD();
-        LogIn login = new LogIn(null, true);
+        /*LogIn login = new LogIn(null, true);
         if(login.iniciarLogIn()) {
         login.setVisible(true);
         } else {
